628069cc930dc54f9c483c3fbfd81bfc03e43120
SteenackersAstrid <49026751+SteenackersAstrid@users.noreply.github.com>
2021-02-12 09:11:22 +0100

    Adding speakers, adding program and updating partners (#37)
    * changed date of submission for Call for Speakers
    
    * changed f
    
    * Added Mono as partner
    
    * fixed mono :)
    
    * Update mono-logo.png
    
    * Try to create a second line of partners
    
    * 2nd line partner logo
    
    * Update speakers, program and sponsors
    
    added speakers
    added sessions
    updated sponsors
    
    * Update index.html
    
    Fix formatting.
    
    * Update webpack.config.js
    
    Allow image file extension in uppercase.
    
    * Update index.html
    
    Add Eventbrite link.
    
    * Update speakers and sessions
    
    Added OSM session
    Added OSM speakers
    Added Pieter C.
    Added Johan R.
    
    * shorten title
    
    Shorten title of Session Aad Versteden
    
    * corrected speaker card Johan Ronsse
    
    * Update index.html
    
    Fix picture path.
    
    * Update index.html
    
    Remove "call for speakers" link.
    
    * Added Mathieu Michel
    
    * Create Mathieu Michel.jpg
    
    Co-authored-by: Jonathan Beliën <jbelien@users.noreply.github.com>
    Co-authored-by: marie devos <mariedevos6@hotmail.com>
    Co-authored-by: Jonathan Beliën <dev@jbelien.be>

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = (env, {mode}) => {
           ]
         },
         {
-          test: /\.(jpe?g|png|svg|webp)$/,
+          test: /\.(jpe?g|png|svg|webp)$/i,
           use: {
             loader: 'url-loader',
             options: {
