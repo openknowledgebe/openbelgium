e1901cf1962d28a2bf8674fec4e9c4e800304431
Jonathan Beliën <dev@jbelien.be>
2022-02-26 14:44:18 +0100

    Update webpack.config.js (formatting)

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const postcssPresetEnv = require('postcss-preset-env');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postcssPresetEnv = require("postcss-preset-env");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports = (env, {mode}) => {
+module.exports = (env, { mode }) => {
   console.log(mode);
   return {
     output: {
-      filename: '[name].[fullhash].js'
+      filename: "[name].[fullhash].js",
     },
     devServer: {
       // overlay: true,
-      hot: true
+      hot: true,
     },
     module: {
       rules: [
@@ -20,92 +20,90 @@ module.exports = (env, {mode}) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: "babel-loader",
+          },
         },
         {
           test: /\.html$/,
           use: [
             {
-              loader: 'html-srcsets-loader',
+              loader: "html-srcsets-loader",
               options: {
-                attrs: [':src', ':srcset']
-              }
-            }
-          ]
+                attrs: [":src", ":srcset"],
+              },
+            },
+          ],
         },
         {
           test: /\.(svg)$/i,
           use: {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 1000,
-              context: './src',
-              name: '[path][name].[ext]',
-              esModule: false // https://stackoverflow.com/questions/59070216/webpack-file-loader-outputs-object-module
-            }
-          }
+              context: "./src",
+              name: "[path][name].[ext]",
+              esModule: false, // https://stackoverflow.com/questions/59070216/webpack-file-loader-outputs-object-module
+            },
+          },
         },
         {
           test: /\.(jpe?g|png|webp)$/i,
           use: [
-            'url-loader',
+            "url-loader",
             {
-              loader: 'webpack-image-resize-loader',
+              loader: "webpack-image-resize-loader",
               options: {
                 width: 350,
-                fileLoader: 'url-loader',
-                fileLoaderOptionsGenerator: (options, existingOptions) => (
-                  {
-                    ...existingOptions,
-                    esModule: false, // https://stackoverflow.com/questions/59070216/webpack-file-loader-outputs-object-module
-                    limit: 1000,
-                    context: './src',
-                    name: '[path][name].[ext]',
-                  }
-                )
+                fileLoader: "url-loader",
+                fileLoaderOptionsGenerator: (options, existingOptions) => ({
+                  ...existingOptions,
+                  esModule: false, // https://stackoverflow.com/questions/59070216/webpack-file-loader-outputs-object-module
+                  limit: 1000,
+                  context: "./src",
+                  name: "[path][name].[ext]",
+                }),
               },
-            }
-          ]
+            },
+          ],
         },
         {
           test: /\.css$/,
           use: [
-            mode === 'production'
+            mode === "production"
               ? MiniCssExtractPlugin.loader
-              : 'style-loader',
-            'css-loader',
-            'resolve-url-loader',
+              : "style-loader",
+            "css-loader",
+            "resolve-url-loader",
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
                 sourceMap: true,
                 postcssOptions: {
                   plugins: [
-                    require('postcss-import'),
-                    postcssPresetEnv({stage: 0})
-                  ]
-                }
-              }
-            }
-          ]
-        }
-      ]
+                    require("postcss-import"),
+                    postcssPresetEnv({ stage: 0 }),
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      ],
     },
     plugins: [
       new HtmlWebPackPlugin({
-        template: './src/index.html',
-        filename: './index.html'
+        template: "./src/index.html",
+        filename: "./index.html",
       }),
       new HtmlWebPackPlugin({
-        template: './src/code-of-conduct.html',
-        filename: './code-of-conduct.html'
+        template: "./src/code-of-conduct.html",
+        filename: "./code-of-conduct.html",
       }),
       new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css'
+        filename: "style.[contenthash].css",
       }),
       new CssMinimizerPlugin(),
       new webpack.HotModuleReplacementPlugin(),
-    ]
+    ],
   };
 };
